6731d745b0ecd92c69f9591f9d4d2aaf4886be85
matt <53015609+MattBilyeu@users.noreply.github.com>
2023-11-15 13:01:43 -0500

    Validation
    Continued work on validation, use of express-validation package

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const { body } = require('express-validator');
 
 const authController = require('../controllers/auth');
+const User = require('../models/user');
 
 const router = express.Router();
 
@@ -8,9 +10,53 @@ router.get('/login', authController.getLogin);
 
 router.get('/signup', authController.getSignup);
 
-router.post('/login', authController.postLogin);
+router.post('/login', 
+    [  
+        body('email')
+            .isEmail()
+            .withMessage('Please enter a valid email')
+            .custom((value) => {
+                User.findOne({ email: value })
+                .then(user => {
+                    if (!user) {
+                        return Promise.reject('No user exists with that email.')
+                    }
+                })
+            }),
+        body('password', 'Please enter an email that is at least 5 characters and has only text and numbers')
+            .isLength({min: 5})
+            .isAlphanumeric(),
+    ],
+    authController.postLogin);
 
-router.post('/signup', authController.postSignup);
+router.post('/signup', 
+    [
+        body('email')
+            .isEmail()
+            .withMessage('Please enter a valid email.')
+            .custom((value, {req}) => {
+                return User.findOne({ email: value })
+                    .then(userDoc => {
+                    if (userDoc) {
+                        return Promise.reject('E-Mail exists already, please pick a different one.')
+                    }})
+            }),
+        body(
+                'password', 
+                'Please enter a password with only numbers and text and at least 5 characters'
+            )
+            .isLength({min: 5})
+            .isAlphanumeric(),
+        body('confirmPassword')
+            .custom((value, {req}) => {
+                if (value !== req.body.password) {
+                    throw new Error('Password and Confirm Password does not match.')
+                }
+                return true
+            })
+    ],
+    authController.postSignup
+);
 
 router.post('/logout', authController.postLogout);
 
