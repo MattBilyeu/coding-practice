a056ccf2980f00dc31fd1b4d77a72ea257353bb4
matt <53015609+MattBilyeu@users.noreply.github.com>
2023-11-21 20:05:17 -0500

    Leetcode Challenge

--- spot-testing/testing.js
+++ spot-testing/testing.js
@@ -1,31 +1,35 @@
-var flat = function (arr, n) {
-    if (n === 0) {
-        return arr
-    };
+Array.prototype.snail = function(rowsCount, colsCount) {
+    if (rowsCount * colsCount !== this.length) {
+        return []
+    }
     let newArray = [];
-    arr.forEach(member => {
-        if (Array.isArray(member)) {
-            newArray = newArray.concat(member)
-        } else {
-            newArray.push(member)
-        }
-    });
-    for (let i = 1; i < n; i++) {
-        let tempArray = [];
-        newArray.forEach(member => {
-            if (Array.isArray(member)) {
-                tempArray = tempArray.concat(member)
-            } else {
-                tempArray.push(member)
-            }
-        });;
-        newArray = tempArray;
+    for (let i = 0; i < rowsCount; i++) {
+        const subArray = [];
+        newArray.push(subArray);
     };
+    if (rowsCount === 1) {
+        newArray = [this];
+        return newArray;
+    }
+    let row = 2;
+    let direction = 1;
+    newArray[0].push(this[0]);
+    for (let i = 1; i < this.length; i++) {
+        let r = row-1;
+        newArray[r].push(this[i]);
+        if (row === rowsCount && i !== this.length - 1 || row === 1 && i !== this.length - 1) {
+            let l = row -1;
+            i++;
+            newArray[l].push(this[i]);
+            direction = direction*-1;
+        }
+        row = row + direction;
+    }
     return newArray;
-};
+}
 
-const arr = [1, 2, 3, [4, 5, 6], [7, 8, [9, 10, 11], 12], [13, 14, 15]];
+let nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25]
+let rowsCount = 5
+let colsCount = 5
 
-console.log(flat(arr, 0));
-
-console.log(flat(arr,1))
+console.log(nums.snail(rowsCount, colsCount))
