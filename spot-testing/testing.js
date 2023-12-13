eab6b812444af4ee7bab3b8441a6394d3db31da1
matt <53015609+MattBilyeu@users.noreply.github.com>
2023-12-13 16:08:47 -0500

    Practice

--- spot-testing/testing.js
+++ spot-testing/testing.js
@@ -1,35 +1,26 @@
-Array.prototype.snail = function(rowsCount, colsCount) {
-    if (rowsCount * colsCount !== this.length) {
-        return []
-    }
-    let newArray = [];
-    for (let i = 0; i < rowsCount; i++) {
-        const subArray = [];
-        newArray.push(subArray);
-    };
-    if (rowsCount === 1) {
-        newArray = [this];
-        return newArray;
-    }
-    let row = 2;
-    let direction = 1;
-    newArray[0].push(this[0]);
-    for (let i = 1; i < this.length; i++) {
-        let r = row-1;
-        newArray[r].push(this[i]);
-        if (row === rowsCount && i !== this.length - 1 || row === 1 && i !== this.length - 1) {
-            let l = row -1;
-            i++;
-            newArray[l].push(this[i]);
-            direction = direction*-1;
+var createCounter = function(init) {
+    let robot = {
+        init: init,
+        startingValue: init,
+        increment: function() {
+            init++;
+            return init
+        },
+        decrement: function() {
+            init--;
+            return init
+        },
+        reset: function() {
+            init = startingValue;
+            return init;
         }
-        row = row + direction;
-    }
-    return newArray;
-}
+    };
+    return robot
+};
 
-let nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25]
-let rowsCount = 5
-let colsCount = 5
+const testBot = createCounter(5);
 
-console.log(nums.snail(rowsCount, colsCount))
+console.log(testBot.increment());
+console.log(testBot.increment());
+console.log(testBot.reset());
+console.log(testBot.increment());
