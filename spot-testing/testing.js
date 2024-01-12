80e98a4af3c20c38eca2a9f4669dd599e47d271f
matt <53015609+MattBilyeu@users.noreply.github.com>
2024-01-12 08:28:14 -0500

    Logging time in code wars
    Logging time practicing algorithms in Code Wars

--- spot-testing/testing.js
+++ spot-testing/testing.js
@@ -1,14 +1,18 @@
-function getCount(str) {
-  let vowelCount = 0;
-  let stringArray = str.split('');
-  let vowels = ['a', 'e', 'i', 'o', 'u'];
-  for (let i = 0; i < stringArray.length; i++) {
-    const foundInVowels = vowels.findIndex(letter => letter === stringArray[i].toLowerCase())
-    if (foundInVowels !== -1) {
-      vowelCount++
+function longest(s1, s2) {
+  const arrayOne = s1.split('');
+  const arrayTwo = s2.split('');
+  const newArray = [];
+  for (let i = 0; i < arrayOne.length; i++) {
+    if (!newArray.includes(arrayOne[i]) && !newArray.includes(arrayOne[i].toLowerCase())) {
+      newArray.push(arrayOne[i])
     }
-  }
-  return vowelCount;
+  };
+  for (let i = 0; i < arrayTwo.length; i++) {
+    if (!newArray.includes(arrayTwo[i]) && !newArray.includes(arrayTwo[i].toLowerCase())) {
+      newArray.push(arrayTwo[i])
+    }
+  };
+  return newArray.sort().join('')
 }
 
 //Logging time spent coding in Codewars
