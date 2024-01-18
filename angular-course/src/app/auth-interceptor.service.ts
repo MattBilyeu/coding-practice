69ba2ab8f42d456f128d50c61668cd4abb4c6ac3
matt <53015609+MattBilyeu@users.noreply.github.com>
2024-01-18 12:44:35 -0500

    Interceptor Finish

--- angular-course/src/app/auth-interceptor.service.ts
+++ angular-course/src/app/auth-interceptor.service.ts
@@ -5,10 +5,6 @@ import { tap } from "rxjs";
 export class AuthInterceptorService implements HttpInterceptor {
     intercept(req: HttpRequest<any>, next: HttpHandler) {
         const modifiedRequest = req.clone({headers: req.headers.append('new-header', 'value')}); //Add API key
-        return next.handle(modifiedRequest).pipe(tap(event => {
-            if (event.type === HttpEventType.Response) {
-                console.log('Response arrived, body data: ', event.body)
-            }
-        }));
+        return next.handle(modifiedRequest);
     }
 }
