e1b2b49452afc6ecb3c93884fca7ec78e52dabb4
matt <53015609+MattBilyeu@users.noreply.github.com>
2024-01-18 13:08:12 -0500

    Finished interceptors, looking at authguards next

--- angular-course/src/app/app.component.ts
+++ angular-course/src/app/app.component.ts
@@ -1,58 +1,16 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
-import { Post } from './post.model';
-import { PostService } from './post.service';
-import { Subscription } from 'rxjs';
+import { AuthService } from './auth/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit, OnDestroy {
-  loadedPosts: Post[] = [];
-  isFetching = false;
-  error: string = null;
-  private errorSub: Subscription
-
-  constructor(private http: HttpClient,
-              private postService: PostService) {}
+export class AppComponent implements OnInit {
+  constructor(private authService: AuthService) {}
 
   ngOnInit() {
-    this.errorSub = this.postService.error.subscribe(errorMessage => {
-      this.error = errorMessage
-    });
-    this.onFetchPosts();
-  }
-
-  onCreatePost(postData: Post) {
-    this.postService.createAndStorePost(postData.title, postData.content);
-  }
-
-  onFetchPosts() {
-    this.isFetching = true;
-    this.postService.fetchPosts().subscribe(posts => {
-        this.isFetching = false;
-        this.loadedPosts = posts;
-      },
-      error => {
-        this.isFetching = false;
-        this.error = error.message
-      });
-  }
-
-  onClearPosts() {
-    this.postService.clearPosts().subscribe(()=> {
-      this.loadedPosts = []
-    });
-  }
-
-  onHandleError() {
-    this.error = null
-  }
-
-  ngOnDestroy() {
-    this.errorSub.unsubscribe()
+    this.authService.autoLogin();
   }
 }
