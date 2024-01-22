12e06e05552068eda17f5d5f336a0eb66a4c30d8
matt <53015609+MattBilyeu@users.noreply.github.com>
2024-01-22 17:55:49 -0500

    Working with template projection

--- angular-testing/src/app/app.component.ts
+++ angular-testing/src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -8,24 +7,5 @@ import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 })
 export class AppComponent {
   title = 'angular-testing';
-  editorContent: string = '';
-
-  editorConfig = {
-    base_url: "/tinymce",
-    suffix: ".min",
-    plugins: "lists link image table wordcount",
-    menubar: true, // Default is true - including because it can be set to false or be set to include only certain tabs
-    toolbar: "formats numlist bullist bold italic alignleft aligncenter alignright justify indent outdent" // Can also customize with what to include
-  }
-
-  constructor(private sanitizer: DomSanitizer){}
-
-  submit() {
-    console.log(this.editorContent);
-    console.log('config data is a typeof: ',typeof this.editorContent);
-  }
-
-  sanitizeHTML(html: string): SafeHtml {
-    return this.sanitizer.bypassSecurityTrustHtml(html); // Needed because Angular won't trust the HTML and will block it
-  }
+  text: string = 'Placeholder';
 }
