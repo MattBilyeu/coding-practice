b30d077657822158ea8339011e8c377e2b9395f3
matt <53015609+MattBilyeu@users.noreply.github.com>
2024-01-25 13:57:13 -0500

    Working with NgRx

--- angular-course/src/app/counter-output/counter-output.component.ts
+++ angular-course/src/app/counter-output/counter-output.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
+import { selectCount, selectDoubleCount } from '../store/counter.selectors';
 
 @Component({
   selector: 'app-counter-output',
@@ -13,9 +14,11 @@ import { Observable } from 'rxjs';
 })
 export class CounterOutputComponent {
   count$: Observable<number>;
+  doubleCount$: Observable<number>;
 
   constructor(private store: Store<{counter: number}>) {
-    this.count$ = store.select('counter');
+    this.count$ = store.select(selectCount);
+    this.doubleCount$ = store.select(selectDoubleCount);
   }
 
 
--- angular-course/src/app/store/counter.actions.ts
+++ angular-course/src/app/store/counter.actions.ts
@@ -1,6 +1,23 @@
-import { createAction, props } from "@ngrx/store";
+import { Action, createAction, props } from "@ngrx/store";
+
+export const INCREMENT = '[Counter] Increment';
+
+export const DECREMENT = '[Counter] Decrement';
 
 export const increment = createAction(
-    '[Counter] Increment',
+    INCREMENT,
     props<{value: number}>()
 );
+
+export const decrement = createAction(
+    DECREMENT,
+    props<{value: number}>()
+)
+
+// export class IncrementAction implements Action {
+//     readonly type: string = INCREMENT;
+
+//     constructor(public value: number) {}
+// }
+
+// export type CounterActions = IncrementAction;
