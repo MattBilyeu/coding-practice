ed1fc8fc00dc542295a6126ca23a99286653dd74
matt <53015609+MattBilyeu@users.noreply.github.com>
2024-01-26 13:16:51 -0500

    NgRx
    Practicing with NgRx

--- angular-course/src/app/store/counter.actions.ts
+++ angular-course/src/app/store/counter.actions.ts
@@ -4,6 +4,19 @@ export const INCREMENT = '[Counter] Increment';
 
 export const DECREMENT = '[Counter] Decrement';
 
+export const INIT = '[Counter] Init';
+
+export const SET = '[Counter] Set';
+
+export const init = createAction(
+    INIT,
+);
+
+export const set = createAction(
+    SET,
+    props<{value: number}>()
+)
+
 export const increment = createAction(
     INCREMENT,
     props<{value: number}>()
---
+++ recipe-project/src/app/shopping-list/store/shopping-list.actions.ts
@@ -0,0 +1,9 @@
+import { createAction, props } from "@ngrx/store";
+import { Ingredient } from "../../shared/ingredient.model";
+
+export const ADD_INGREDIENT = '[Shopping List] Add Ingredient';
+
+export const addIngredient = createAction(
+    ADD_INGREDIENT,
+    props<{ingredient: Ingredient}>()
+)
