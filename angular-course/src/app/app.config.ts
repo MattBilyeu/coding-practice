6eb2c43a2b255992063017c4faf4e0802f358210
matt <53015609+MattBilyeu@users.noreply.github.com>
2024-01-25 20:56:26 -0500

    Working with NgRx

--- angular-course/src/app/app.config.ts
+++ angular-course/src/app/app.config.ts
@@ -5,7 +5,9 @@ import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideStore } from '@ngrx/store';
 import { counterReducer } from './store/counter.reducer';
+import { provideEffects } from '@ngrx/effects';
+import { CounterEffects } from './store/counter.effects';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration(), provideStore({counter: counterReducer})]
+  providers: [provideRouter(routes), provideClientHydration(), provideStore({ counter: counterReducer }), provideEffects([CounterEffects])]
 };
