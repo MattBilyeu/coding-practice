f1ceacbf8b3e89686971da205ef56ab25354661e
matt <53015609+MattBilyeu@users.noreply.github.com>
2024-02-04 19:01:15 -0500

    Training
    Training on Data Structures and Algorithms

--- spot-testing/testing.js
+++ spot-testing/testing.js
@@ -1,26 +1,28 @@
-let beginning = -Infinity;
-
-let current = 2024;
-
-let failureString;
-
-//Can we arrive at current by adding to negative infinity?
-
-function arriveAtCurrent() {
-  while (beginning !== current) {
-    beginning = beginning + 1;
-    if (beginning === current) {
-      return `We did it!!`
-    } else {
-      if (!failureString) {
-        failureString = 'Not yet...';
-        console.log(failureString)
-      }
+function rot13(str) {
+  const alpha = 'abcdefghijklmnopqrstuvwxyz';
+  const alphaArray = alpha.split('');
+  let stringArray = str.split('');
+  let returnArray = [];
+  for (let i = 0; i < stringArray.length; i++) {
+    let index = alphaArray.findIndex(l => l === stringArray[i].toLowerCase());
+    console.log(index);
+    if (index + 13 > 25) {
+      index -= 13
+    } else if (index !== -1) {
+      index += 13
     }
-  }
-}
+    console.log(index);
+    if (index !== -1) {
+      if (stringArray[i] === stringArray[i].toUpperCase()) {
+        returnArray.push(alphaArray[index].toUpperCase())
+      } else {
+        returnArray.push(alphaArray[index])
+      }
+    } else {
+      returnArray.push(stringArray[i])
+    }
+  };
+  return returnArray.join('')
+};
 
-console.log('Trying to arrive at present from negative infinity ...');
-
-console.log(arriveAtCurrent());
-
+console.log(rot13('rkn'));
