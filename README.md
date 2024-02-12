aa5237b71956500e1629f25a7d5e54aad0bcb9aa
matt <53015609+MattBilyeu@users.noreply.github.com>
2024-02-12 13:59:03 -0500

    REST API
    Studying RESTful API creation

--- README.md
+++
@@ -1,4 +0,0 @@
-# coding-practice
-Practice Repository
-
-This repository should not be perused as indicative of my skill.  It contains tutorial code or other testing.
--- recipe-project/src/app/shopping-list/store/shopping-list.actions.ts
+++
@@ -1,9 +0,0 @@
-import { createAction, props } from "@ngrx/store";
-import { Ingredient } from "../../shared/ingredient.model";
-
-export const ADD_INGREDIENT = '[Shopping List] Add Ingredient';
-
-export const addIngredient = createAction(
-    ADD_INGREDIENT,
-    props<{ingredient: Ingredient}>()
-)
